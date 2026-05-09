d10f58942fdbe068d7500f79b5b4ea6ee43772e4
VeronicaTeodorof <vteodorof@rocketmail.com>
2026-05-09 08:52:20 +0100

    chore: test authentication app views

--- gotit/urls.py
+++ gotit/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from authentication import views as authentication_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('authentication/', authentication_views.authenticate, name="authentication"), 
 ]
