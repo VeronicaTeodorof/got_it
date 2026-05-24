c7470757c4baf2a3a1064f16ebf5e95d958cb626
VeronicaTeodorof <vteodorof@rocketmail.com>
2026-05-24 18:37:12 +0100

    style: add basic styles to sources.html

--- gotit/urls.py
+++ gotit/urls.py
@@ -15,11 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path, include
 
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path("", include("notes.urls"), name="notes-urls"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
