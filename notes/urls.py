c34dd077ce86630f277d85e1cc72d16e339b798c
VeronicaTeodorof <vteodorof@rocketmail.com>
2026-05-25 09:48:55 +0100

    feat: add dashboard sidebar layout and update source and  source detail templates

--- notes/urls.py
+++ notes/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('sources/', views.sources_list, name='sources-list'),
-    path('<int:source_pk>/', views.source_detail, name='source-detail'),
+    path(
+        'sources/<int:source_pk>/', views.source_detail, name='source-detail'
+        ),
 ]
